952ca46c2709e28357ac0e7922428d91622c4119
FrogCpp <aleksei.a.mikhalev@gmail.com>
2025-09-21 17:23:31 +0300

    fine

--- BotCode/TimeController.cs
+++ BotCode/TimeController.cs
@@ -8,9 +8,58 @@ namespace BotFix
 {
     internal class TimeController
     {
-        public TimeController() 
+        private DateTime targetTime;
+        private Timer checkTimer;
+        private bool isFunctionExecutedToday = false;
+        private TelegramController tgc;
+        public TimeController(float time, TelegramController tgControl) 
         {
+            //Console.WriteLine(DateTime.Now);
+            targetTime = DateTime.Today.AddHours(time);
 
+            checkTimer = new Timer(CheckTime, null, TimeSpan.Zero, TimeSpan.FromMinutes(1));
+
+            tgc = tgControl;
+        }
+
+        private void CheckTime(object state)
+        {
+            DateTime now = DateTime.Now;
+
+            if (!isFunctionExecutedToday && now >= targetTime)
+            {
+                mainFunc();
+
+                isFunctionExecutedToday = true;
+
+                targetTime = targetTime.AddDays(1);
+            }
+
+            if (now.Hour == 0 && now.Minute == 0)
+            {
+                isFunctionExecutedToday = false;
+            }
+        }
+
+        private void mainFunc()
+        {
+            using (var f = new FileManager("/Users.json"))
+            {
+                foreach (var i in f.MyUsers)
+                {
+                    var a = i.MyLessonsList;
+                    int dayNumber = ((int)DateTime.Now.DayOfWeek + 6) % 7;
+                    if (a.Count >= dayNumber)
+                    {
+                        tgc.SendMessage($"{i.usrName}, вот твое расписание на завтрашний день!\n*леша не забудь починить это, после того, как егор исправит сплитер*", i.userID);
+                    }
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            checkTimer?.Dispose();
         }
     }
 }
