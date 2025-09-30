8fa2321cd353a5522b1898afe22d271462200d20
a <161460644+FrogCpp@users.noreply.github.com>
2025-09-30 20:16:50 +0300

    Revert "сука"
    This reverts commit 0e61dbcde14175f1a7d4ea8c203f03ef052466b0.

--- BotCode/TimeController.cs
+++ BotCode/TimeController.cs
@@ -51,11 +51,6 @@ namespace BotFix
                     int dayNumber = ((int)DateTime.Now.DayOfWeek + 6) % 7;
                     if (a.Count >= dayNumber)
                     {
-                        Console.WriteLine(a);
-                    }
-
-                    if (LessonsLst.Count >= dayNumber)
-                    {
                         tgc.SendMessage($"{i.usrName}, вот твое расписание на завтрашний день!\n*леша не забудь починить это, после того, как егор исправит сплитер*", i.userID);
                     }
                 }
@@ -65,45 +60,6 @@ namespace BotFix
         public void Dispose()
         {
             checkTimer?.Dispose();
-        }
-
-        static private Weekday IntToWeekday(int input)
-        {
-            return input switch
-            {
-                1 => Weekday.Monday,
-                2 => Weekday.Tuesday,
-                3 => Weekday.Wednesday,
-                4 => Weekday.Thursday,
-                5 => Weekday.Friday,
-                6 => Weekday.Saturday,
-                0 => Weekday.Sunday,
-                _ => Weekday.Undefined
-            };
-        }
-
-        private List<DaySchedule> SplitMyString(string text)
-        {
-            List<DaySchedule> LessonsLst = [];
-            List<string> a = text.Split("*\n").ToList<string>();
-            for (int j = 0; j < a.Count; j++)
-            {
-                foreach (string lesson in a[j].Split('\n'))
-                {
-                    DaySchedule c = new DaySchedule();
-                    if (lesson.Contains(' '))
-                    {
-                        var b = lesson.Split(' ');
-                        c.AddSubject(new Subject(b[0], uint.Parse(b[1])));
-                    }
-                    else
-                    {
-                        c.AddSubject(new Subject(lesson));
-                    }
-                    LessonsLst.Add(c);
-                }
-            }
-            return LessonsLst;
         }
     }
 }
